49ce34b4895b70d023db7508a92436860cf63315
Marcus Birkenkrahe <377079+birkenkrahe@users.noreply.github.com>
2022-02-17 16:01:43 -0600

    c++ in org mode

---
+++ emacs/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int i;
+  cout << "Please enter an integer value: ";
+  cin >> i;
+  cout << "The value you entered is " << i;
+  return 0;
+}
