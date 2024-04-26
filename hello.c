4c67f1f6396eb342c1190a8a09ef7a15d7bc2959
Marcus Birkenkrahe <377079+birkenkrahe@users.noreply.github.com>
2024-04-26 09:51:56 -0500

    Update from dell

---
+++ hello.c
@@ -0,0 +1,13 @@
+// input output macro
+#include <stdio.h>
+// function definition
+void hello_world(void)
+  {
+    printf("Hello world\n");
+  }
+// main function
+int main(void) {
+  // function call
+  hello_world();
+  return 0;
+  }
